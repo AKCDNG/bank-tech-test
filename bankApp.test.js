1c704a9e894db24589ad9ece86e2c67715e98f3c
Anish <anishkakaiya@outlook.com>
2022-06-20 13:27:23 +0100

    removed brackets from bank.statement to return properly

--- bankApp.test.js
+++ bankApp.test.js
@@ -5,6 +5,6 @@ describe('Bank App Testing', () => {
     const bank = new bankApp();
 
     bank.deposit(100);
-    expect(bank.statement()).toEqual(['date || credit || debit || balance', '19/07/2022 || 100.00 || || 100.00'])
+    expect(bank.statement).toEqual(['date || credit || debit || balance', '19/07/2022 || 100.00 || || 100.00'])
   })
 })
