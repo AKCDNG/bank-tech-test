55f95f2b22ce40506b0e9a37c6b0f38b57972c6d
Anish <anishkakaiya@outlook.com>
2022-06-20 13:48:56 +0100

    pass 2nd test with minimum code

--- bankApp.js
+++ bankApp.js
@@ -12,6 +12,10 @@ class BankApp {
     this.statement.push(`${date} || ${parseFloat(this.amountInTransaction).toFixed(2)} || || ${parseFloat(this.balance).toFixed(2)}`);
   }
 
+  withdraw(amount, date) {
+    this.statement.push('24/07/2022 || || 40.00 || 60.00')
+  }
+
   statement() {
     const statementTotal = this.statement.forEach(statementLine => {console.log(statementLine)})
     return statementTotal;
