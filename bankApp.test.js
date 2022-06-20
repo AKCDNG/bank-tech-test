3f6048cfe68d7aa859553b574e071efe257aeb8d
Anish <anishkakaiya@outlook.com>
2022-06-20 16:54:29 +0100

    update tests to have header at the top and reverse list so recent transactions are first

--- bankApp.test.js
+++ bankApp.test.js
@@ -5,7 +5,7 @@ describe('Bank App Testing', () => {
     const bank = new BankApp();
 
     bank.deposit(100, '19/07/2022');
-    expect(bank.statement).toEqual(['date || credit || debit || balance', '19/07/2022 || 100.00 || || 100.00'])
+    expect(bank.viewStatement()).toEqual(['date || credit || debit || balance', '19/07/2022 || 100.00 || || 100.00'])
   })
 
   it('Shows withdrawal of £40 on 24st July 2022 after deposit of £100', () => {
@@ -13,19 +13,19 @@ describe('Bank App Testing', () => {
 
     bank.deposit(100, '19/07/2022');
     bank.withdraw(40, '24/07/2022');
-    expect(bank.statement).toEqual(['date || credit || debit || balance', '19/07/2022 || 100.00 || || 100.00', '24/07/2022 || || 40.00 || 60.00'])
+    expect(bank.viewStatement()).toEqual(['date || credit || debit || balance', '24/07/2022 || || 40.00 || 60.00', '19/07/2022 || 100.00 || || 100.00'])
   })
 
-  it('Tests the given criteria, (original criteria is within README', () => {
+  it('Tests the given criteria, (original criteria is within README) with reversed list of transactions', () => {
     const bank = new BankApp();
 
     bank.deposit(1000, '10/01/2023');
     bank.deposit(2000, '13/01/2023');
     bank.withdraw(500, '14/01/2023');
 
-    expect(bank.statement).toEqual(['date || credit || debit || balance',
-    '10/01/2023 || 1000.00 || || 1000.00',
+    expect(bank.viewStatement()).toEqual(['date || credit || debit || balance',
+    '14/01/2023 || || 500.00 || 2500.00',
     '13/01/2023 || 2000.00 || || 3000.00',
-    '14/01/2023 || || 500.00 || 2500.00'])
+    '10/01/2023 || 1000.00 || || 1000.00'])
   })
 })
