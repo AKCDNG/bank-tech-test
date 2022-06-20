9994bb11cdc5f6daabfa337332ddfecc70ce7766
Anish <anishkakaiya@outlook.com>
2022-06-20 13:02:25 +0100

    pass first test without refactoring for £100 deposit

---
+++ bankApp.js
@@ -0,0 +1,12 @@
+class bankApp {
+
+  deposit(amount) {
+  }
+
+  statement() {
+    return ['date || credit || debit || balance', '19/07/2022 || 100.00 || || 100.00']
+  }
+
+}
+
+module.exports = bankApp;
