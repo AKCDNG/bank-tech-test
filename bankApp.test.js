1287b21d05e3ceaae37c80863902296012962bcf
Anish <anishkakaiya@outlook.com>
2022-06-20 13:33:29 +0100

    adjust function test to accept a custom date as a string

--- bankApp.test.js
+++ bankApp.test.js
@@ -4,7 +4,7 @@ describe('Bank App Testing', () => {
   it('Deposits £100 and show statement with £100 credit and £100 balance on 19/07/2022', () => {
     const bank = new bankApp();
 
-    bank.deposit(100);
+    bank.deposit(100, '19/07/2022');
     expect(bank.statement).toEqual(['date || credit || debit || balance', '19/07/2022 || 100.00 || || 100.00'])
   })
 })
