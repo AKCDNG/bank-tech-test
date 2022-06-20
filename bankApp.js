3b10e07d2bce2f6cf3dfe08a529345f5d2183bf5
Anish <anishkakaiya@outlook.com>
2022-06-20 14:37:38 +0100

    lint code to be cleaner

--- bankApp.js
+++ bankApp.js
@@ -1,28 +1,26 @@
 class BankApp {
-
-  constructor() {
-    this.statement = ['date || credit || debit || balance'];
+  constructor () {
+    this.statement = ['date || credit || debit || balance']
     this.balance = 0
     this.amountInTransaction = 0
   }
 
-  deposit(amount, date) {
-    this.balance += amount;
+  deposit (amount, date) {
+    this.balance += amount
     this.amountInTransaction = amount
-    this.statement.push(`${date} || ${parseFloat(this.amountInTransaction).toFixed(2)} || || ${parseFloat(this.balance).toFixed(2)}`);
+    this.statement.push(`${date} || ${parseFloat(this.amountInTransaction).toFixed(2)} || || ${parseFloat(this.balance).toFixed(2)}`)
   }
 
-  withdraw(amount, date) {
-    this.balance -= amount;
+  withdraw (amount, date) {
+    this.balance -= amount
     this.amountInTransaction = amount
-    this.statement.push(`${date} || || ${parseFloat(this.amountInTransaction).toFixed(2)} || ${parseFloat(this.balance).toFixed(2)}`);
+    this.statement.push(`${date} || || ${parseFloat(this.amountInTransaction).toFixed(2)} || ${parseFloat(this.balance).toFixed(2)}`)
   }
 
-  statement() {
-    const statementTotal = this.statement.forEach(statementLine => {console.log(statementLine)})
-    return statementTotal;
+  statement () {
+    const statementTotal = this.statement.forEach(statementLine => { console.log(statementLine) })
+    return statementTotal
   }
-
 }
 
-module.exports = BankApp;
+module.exports = BankApp
