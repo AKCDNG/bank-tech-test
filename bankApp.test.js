766dc695c6ea50428dd8abdfc824fd7f6b06ddf6
Anish <anishkakaiya@outlook.com>
2022-06-20 18:21:48 +0100

    remove unwanted dates in arguments

--- bankApp.test.js
+++ bankApp.test.js
@@ -14,17 +14,17 @@ describe('Bank App Testing', () => {
   it('Shows withdrawal of £40 on 19th July 2022 after deposit of £100', () => {
     const bank = new BankApp();
 
-    bank.deposit(100, '19/07/2022');
-    bank.withdraw(40, '19/07/2022');
+    bank.deposit(100);
+    bank.withdraw(40);
     expect(bank.viewStatement()).toEqual(['date || credit || debit || balance', '19/07/2022 || || 40.00 || 60.00', '19/07/2022 || 100.00 || || 100.00'])
   })
 
   it('Tests the given criteria, (original criteria is within README) with reversed list of transactions', () => {
     const bank = new BankApp();
 
-    bank.deposit(1000, '19/07/2022');
-    bank.deposit(2000, '19/07/2022');
-    bank.withdraw(500, '19/07/2022');
+    bank.deposit(1000);
+    bank.deposit(2000);
+    bank.withdraw(500);
 
     expect(bank.viewStatement()).toEqual(['date || credit || debit || balance',
     '19/07/2022 || || 500.00 || 2500.00',
