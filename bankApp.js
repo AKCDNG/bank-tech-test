e2a0ac54dc59d3f7f20aeaef65ad47a0df3bd393
Anish <anishkakaiya@outlook.com>
2022-06-20 13:28:22 +0100

    refactor - code works, still hardcoding date

--- bankApp.js
+++ bankApp.js
@@ -1,10 +1,19 @@
 class bankApp {
 
+  constructor() {
+    this.statement = ['date || credit || debit || balance'];
+    this.balance = 0
+    this.amountInTransaction = 0
+  }
+
   deposit(amount) {
+    this.balance += amount;
+    this.amountInTransaction = amount
+    this.statement.push(`${'19/07/2022'} || ${parseFloat(this.amountInTransaction).toFixed(2)} || || ${parseFloat(this.balance).toFixed(2)}`);
   }
 
   statement() {
-    return ['date || credit || debit || balance', '19/07/2022 || 100.00 || || 100.00']
+    return this.statement;
   }
 
 }
