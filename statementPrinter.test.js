9ab5ecd849878f9a69eaaf6c8c0114820d20c980
Anish <anishkakaiya@outlook.com>
2022-06-24 12:03:59 +0100

    adjust tests to pass

--- statementPrinter.test.js
+++ statementPrinter.test.js
@@ -3,11 +3,12 @@ const statementPrinter = require('./statementPrinter')
 describe('Statement Calculator Testing', () => {
   it('Calculates a statement in the correct format', () => {
     const statement = new statementPrinter
-    statement.bankStatement.push('19/07/2022 || 10.00 || || 10.00')
-    statement.bankStatement.push('19/07/2022 || 30.00 || || 40.00')
-    statement.bankStatement.push('19/07/2022 || || 10.00 || 30.00')
+    const bankStatement = []
+    bankStatement.push('19/07/2022 || 10.00 || || 10.00')
+    bankStatement.push('19/07/2022 || 30.00 || || 40.00')
+    bankStatement.push('19/07/2022 || || 10.00 || 30.00')
 
-    expect(statement.calculateStatement()).toEqual(['date || credit || debit || balance', 
+    expect(statement.calculateStatement(bankStatement)).toEqual(['date || credit || debit || balance', 
     '19/07/2022 || || 10.00 || 30.00', 
     '19/07/2022 || 30.00 || || 40.00', 
     '19/07/2022 || 10.00 || || 10.00'])
